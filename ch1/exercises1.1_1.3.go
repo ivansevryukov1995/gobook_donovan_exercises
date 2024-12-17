ebb1580aeb680d2b81a77a06fb1c6af10b5f9124
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2024-12-17 16:54:01 +0300

    exercises1

---
+++ ch1/exercises1.1_1.3.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func exercises1_1(s []string) {
+	fmt.Println(s[0])
+}
+
+func exercises1_2(s []string) {
+	for i := 0; i < len(s); i++ {
+		fmt.Println(i, s[i])
+	}
+}
+
+func exercises1_3Joint(s []string) {
+	fmt.Println(strings.Join(s, " "))
+}
+
+func main() {
+
+	s := os.Args //example, $ go run exercises1.1_1.3.go -f -d
+
+	exercises1_1(s)
+
+	exercises1_2(s)
+
+	start := time.Now()
+	exercises1_2(s)
+	end1 := time.Since(start).Microseconds()
+
+	start = time.Now()
+	exercises1_3Joint(s)
+	end2 := time.Since(start).Microseconds()
+
+	fmt.Printf("Usual: %v, with strings.Join: %v", end1, end2) //approximately Usual: 528, with strings.Join: 0
+
+}
---
+++ ch1/go.mod
@@ -0,0 +1,3 @@
+module gobook_donovan_exercises/ch1
+
+go 1.23.2
